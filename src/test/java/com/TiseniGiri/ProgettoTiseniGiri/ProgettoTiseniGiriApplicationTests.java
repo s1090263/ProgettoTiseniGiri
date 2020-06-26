21f9a15598d23402286d4123580d8313e5031e82
Matteo Giri <s1090263@studenti.univpm.it>
2020-06-26 10:19:06 +0200

    primo commit

---
+++ src/test/java/com/TiseniGiri/ProgettoTiseniGiri/ProgettoTiseniGiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiseniGiri.ProgettoTiseniGiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoTiseniGiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
