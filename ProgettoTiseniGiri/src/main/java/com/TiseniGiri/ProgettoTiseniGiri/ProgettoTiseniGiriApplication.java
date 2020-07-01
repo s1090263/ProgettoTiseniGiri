cf398a08a4658bb7be5708d33d3800755fde28ff
Matteo Giri <s1090263@studenti.univpm.it>
2020-07-01 12:22:24 +0200

    Repush main

--- ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/ProgettoTiseniGiriApplication.java
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/ProgettoTiseniGiriApplication.java
@@ -1,21 +1,14 @@
 package com.TiseniGiri.ProgettoTiseniGiri;
 
-import java.net.URLConnection;
-import java.util.List;
-import java.util.ArrayList;
+
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.TiseniGiri.ProgettoTiseniGiri.Utility.*;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.TiseniGiri.ProgettoTiseniGiri.Model.*;
-
 @SpringBootApplication
 public class ProgettoTiseniGiriApplication {
 
-	public static void main(String[] args) throws JsonMappingException, JsonProcessingException{
-		SpringApplication.run(ProgettoTiseniGiriApplication.class, args);		      		   			
+	public static void main(String[] args){
+		SpringApplication.run(ProgettoTiseniGiriApplication.class, args);			
 	}
 }
