d190a2744873fa2236ae57ef5dbe6a2d4a19d38b
Matteo Giri <s1090263@studenti.univpm.it>
2020-07-01 11:03:08 +0200

    prima implementazione controller

---
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Controller/Controller.java
@@ -0,0 +1,27 @@
+package com.TiseniGiri.ProgettoTiseniGiri.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.TiseniGiri.ProgettoTiseniGiri.Services.GeneralService;
+import com.TiseniGiri.ProgettoTiseniGiri.Utility.UrlSetter;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+@RestController
+public class Controller {
+	
+	@GetMapping("/tweets")
+	public ResponseEntity<Object> getTweets(@RequestParam(name="topic", defaultValue="@UnivPoliMarche") String topic, @RequestParam(name="num", defaultValue="100") int num) throws JsonMappingException, JsonProcessingException{
+		return new ResponseEntity<>(GeneralService.getTweets(UrlSetter.getUrl(topic,num)),HttpStatus.OK);
+	}
+	
+//	@PostMapping("/tweets/filter")
+//	public ResponseEntity<Object> getFilteredTweets(@RequestParam(name="topic", defaultValue="@UnivPoliMarche") String topic, @RequestParam(name="num", defaultValue="100") int num, @RequestBody String filters){
+//		return new ResponseEntity<>();
+//	}
+}
