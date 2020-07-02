5dc139b72f809b1f57e13f9a16e89cf120c0a533
Matteo Giri <s1090263@studenti.univpm.it>
2020-07-02 10:32:32 +0200

    Eliminate classi superflue

--- ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Filter/Interfaces/Filter.java
+++
@@ -1,9 +0,0 @@
-package com.TiseniGiri.ProgettoTiseniGiri.Filter.Interfaces;
-
-import java.util.List;
-import com.TiseniGiri.ProgettoTiseniGiri.Model.Tweet;
-
-public interface Filter<T> {
-	
-	public List<Tweet> generalFilter(List<Tweet> list, Integer x);
-}
