cd8c81134f95a74ff4841f9994ca43f8333d6b53
Matteo Giri <s1090263@studenti.univpm.it>
2020-07-02 13:11:47 +0200

    aggiornato url setter

--- ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Utility/UrlSetter.java
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Utility/UrlSetter.java
@@ -1,7 +1,7 @@
 package com.TiseniGiri.ProgettoTiseniGiri.Utility;
 
 public class UrlSetter {
-	private static String url = "https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/search/tweets.json?q=univpm&count=100" ;
+	private static String url = "https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/search/tweets.json?q=@UnivPoliMarche&count=100" ;
 
 	//getter con topic e numero, restituisce il numero di tweets richiesto
 	public static String getUrl(String topic, int num) {
