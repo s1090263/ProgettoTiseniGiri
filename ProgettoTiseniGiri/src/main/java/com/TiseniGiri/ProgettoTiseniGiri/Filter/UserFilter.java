fc61ee34e7041ef72f154a785143cd17a4236698
Lorenzo Tiseni <s1087649@studenti.univpm.it>
2020-07-03 22:00:25 +0200

    Aggiunto filtro per nome Utente

---
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Filter/UserFilter.java
@@ -0,0 +1,21 @@
+package com.TiseniGiri.ProgettoTiseniGiri.Filter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.TiseniGiri.ProgettoTiseniGiri.Filter.Interfaces.StringFilter;
+import com.TiseniGiri.ProgettoTiseniGiri.Model.Tweet;
+
+public class UserFilter implements StringFilter {
+
+	@Override
+	public List<Tweet> stringSearch(List<Tweet> list, Object word) {
+		ArrayList<Tweet> list1 = new ArrayList<Tweet>();
+		for(Tweet t: list) {
+		if(t.getUser().getName().equals(word))
+		list1.add(t);
+		}
+		return list1;
+	}
+
+}
