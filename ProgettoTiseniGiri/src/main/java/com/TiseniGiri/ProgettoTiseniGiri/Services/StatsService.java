c934200a2e8191a52ef1a6824f9787ce68a99d2a
Matteo Giri <s1090263@studenti.univpm.it>
2020-07-03 11:20:53 +0200

    aggiunto servizio per i filtered stats

--- ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Services/StatsService.java
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Services/StatsService.java
@@ -11,6 +11,7 @@ import com.TiseniGiri.ProgettoTiseniGiri.Model.Tweet;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class StatsService extends GeneralService {
 	
@@ -35,9 +36,30 @@ public class StatsService extends GeneralService {
 		return completedStats;
 	}
 	
-	public static int getFilteredStats(String url,String filters) throws JsonMappingException, JsonProcessingException {
-		List<Tweet> tweets = getTweets(url);		
-		return 0;
+	public static Map<String, List<String>> getFilteredStats(String url,String requests) throws JsonMappingException, JsonProcessingException, ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		
+		List<Tweet> tweets = getTweets(url);
+		Map<String,List<String>> filters = null;
+		Map<String,String> stats = null;
+		
+		ObjectMapper mapper = new ObjectMapper();
+		final ObjectNode node = new ObjectMapper().readValue(requests, ObjectNode.class);
+		filters = mapper.readValue(node.get("filters").toString(),Map.class);
+		stats = mapper.readValue(node.get("stats").toString(),Map.class);
+		String filterString = mapper.writeValueAsString(filters);
+		
+		List<Tweet> filteredTweets = FilterService.getFilteredTweets(url, filterString);
+		Map<String, List<String>> completedStats = new HashMap<String, List<String>>(); //mappa delle statistiche eseguite
+		
+		for (Map.Entry<String, String> entry : stats.entrySet()) {
+		    String parameter = entry.getKey(); //parametro su cui fare statistica
+		    String stat = entry.getValue();	//tipo di statistica da fare
+		    List<String> list = new ArrayList<String>();
+		    list.add(stat);
+		    list.add(String.valueOf(Execute(tweets,parameter,stat)));
+		    completedStats.put(parameter, list);
+		}
+		return completedStats;
 	}
 	
 	public static int Execute(List<Tweet> tweets, String parameter, String stat) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
--- ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Stats/Interfaces/Stat.java
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Stats/Interfaces/Stat.java
@@ -1,5 +1,9 @@
 package com.TiseniGiri.ProgettoTiseniGiri.Stats.Interfaces;
 
-public interface Stat {
+import java.util.List;
 
+import com.TiseniGiri.ProgettoTiseniGiri.Model.Tweet;
+
+public interface Stat {
+	public Integer stat_general(List<Tweet> list);
 }
