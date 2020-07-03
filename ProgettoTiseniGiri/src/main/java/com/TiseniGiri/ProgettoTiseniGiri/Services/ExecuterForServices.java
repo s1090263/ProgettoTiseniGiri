ef8502e23fb158a73f0d8bf93f238de9443254ec
Matteo Giri <s1090263@studenti.univpm.it>
2020-07-03 13:31:07 +0200

    generalizzato executer

--- ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Services/ExecuterForServices.java
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Services/ExecuterForServices.java
@@ -7,19 +7,21 @@ import java.util.List;
 import com.TiseniGiri.ProgettoTiseniGiri.Model.Tweet;
 
 public class ExecuterForServices {
-	
-	public List<Tweet> ExecuteFilter(List<Tweet> tweets, String parameter, String filter, int num) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+	public List<Tweet> ExecuteFilter(List<Tweet> tweets, String parameter, String filter, Object par) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
 		Class<?> typeClass = Class.forName("com.TiseniGiri.ProgettoTiseniGiri.Filter." + parameter + "Filter");
 		Object typeFilter = typeClass.getDeclaredConstructor().newInstance();
 		Method method = typeClass.getDeclaredMethod(filter, List.class, int.class);				
-		List<Tweet> filteredTweets= (List<Tweet>) method.invoke(typeFilter, tweets, num);
+		List<Tweet> filteredTweets= (List<Tweet>) method.invoke(typeFilter, tweets, par);
 		return filteredTweets;
 	}
-	public List<Tweet> ExecuteFilter(List<Tweet> tweets, String parameter, String filter, int num1, int num2) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+	
+	public List<Tweet> ExecuteFilter(List<Tweet> tweets, String parameter, String filter, Object par1, Object par2) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
 		Class<?> typeClass = Class.forName("com.TiseniGiri.ProgettoTiseniGiri.Filter." + parameter + "Filter");
 		Object typeFilter = typeClass.getDeclaredConstructor().newInstance();
 		Method method = typeClass.getDeclaredMethod(filter, List.class, int.class,int.class);				
-		List<Tweet> filteredTweets= (List<Tweet>) method.invoke(typeFilter, tweets, num1,num2);
+		List<Tweet> filteredTweets= (List<Tweet>) method.invoke(typeFilter, tweets, par1,par2);
 		return filteredTweets;
 	}
+	
+	int ciao;
 }
