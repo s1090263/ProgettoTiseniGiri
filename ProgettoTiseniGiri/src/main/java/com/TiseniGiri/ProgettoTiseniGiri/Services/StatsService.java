5aa7f55bb0f8ccccf9e0844d057fd0e2dbd96db4
Matteo Giri <s1090263@studenti.univpm.it>
2020-07-04 16:30:29 +0200

    Aggiunto metodo statCycler

--- ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Services/StatsService.java
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Services/StatsService.java
@@ -25,27 +25,12 @@ public class StatsService extends GeneralService {
 		Map<String,List<Object>> request = null;
 		ObjectMapper mapper = new ObjectMapper();
 		request = mapper.readValue(stats,Map.class);
-		
-		Map<String, List<String>> completedStats = new HashMap<String, List<String>>(); //mappa delle statistiche eseguite
-		for (Map.Entry<String, List<Object>> entry : request.entrySet()) {
-		    String parameter = entry.getValue().get(0).toString(); //parametro su cui fare statistica
-		    String stat = entry.getValue().get(1).toString();	//tipo di statistica da fare
-		    List<String> list = new ArrayList<String>();
-		    list.add(parameter);
-		    list.add(stat);
-		    if(entry.getValue().size() == 2)
-		    	list.add(String.valueOf(executer.ExecuteStat(tweets,parameter,stat)));
-		    else
-		    	list.add(String.valueOf(executer.ExecuteStat(tweets,parameter,stat,  entry.getValue().get(2))));
-		    completedStats.put(entry.getKey(), list);
-		}
-		
-		return completedStats;
+			
+		return statCycler(request,tweets);
 	}
 	
 	public static Map<String, List<String>> getFilteredStats(String url,String requests) throws JsonMappingException, JsonProcessingException, ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
 		
-		List<Tweet> tweets = getTweets(url);
 		Map<String,List<Object>> filters = null;
 		Map<String,List<Object>> stats = null;
 		
@@ -54,23 +39,9 @@ public class StatsService extends GeneralService {
 		filters = mapper.readValue(node.get("filters").toString(),Map.class);
 		stats = mapper.readValue(node.get("stats").toString(),Map.class);
 		String filterString = mapper.writeValueAsString(filters);
-		
 		List<Tweet> filteredTweets = FilterService.getFilteredTweets(url, filterString);
-		Map<String, List<String>> completedStats = new HashMap<String, List<String>>(); //mappa delle statistiche eseguite
 		
-		for (Map.Entry<String, List<Object>> entry : stats.entrySet()) {
-		    String parameter = entry.getValue().get(0).toString(); //parametro su cui fare statistica
-		    String stat = entry.getValue().get(1).toString();	//tipo di statistica da fare
-		    List<String> list = new ArrayList<String>();
-		    list.add(parameter);
-		    list.add(stat);
-		    if(entry.getValue().size() == 2)
-		    	list.add(String.valueOf(executer.ExecuteStat(filteredTweets,parameter,stat)));
-		    else
-		    	list.add(String.valueOf(executer.ExecuteStat(filteredTweets,parameter,stat,  entry.getValue().get(2))));
-		    completedStats.put(entry.getKey(), list);
-		}
-		return completedStats;
+		return statCycler(stats,filteredTweets);
 	}
 	
 	public static Map<String, List<String>> statCycler(Map<String,List<Object>> stats, List<Tweet> filteredTweets) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException{
