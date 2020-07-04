29bd506318f07064642d401ee61fa5aecc70ecdc
Matteo Giri <s1090263@studenti.univpm.it>
2020-07-04 11:19:41 +0200

    Aggiunta eccezione per il between

--- ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Exceptions/ExtremesException.java
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Exceptions/ExtremesException.java
@@ -1,5 +1,6 @@
 package com.TiseniGiri.ProgettoTiseniGiri.Exceptions;
 
+//eccezione che gestisce il between
 public class ExtremesException extends RuntimeException{
 	
 	private static final long serialVersionUID = 1L;
