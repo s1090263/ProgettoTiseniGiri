dda1a952edc1eb77057393595c683aa7243226be
Matteo Giri <s1090263@studenti.univpm.it>
2020-07-04 11:13:24 +0200

    Aggiunta eccezione per il between

--- ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Exceptions/ExtremesException.java
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Exceptions/ExtremesException.java
@@ -1,6 +1,6 @@
 package com.TiseniGiri.ProgettoTiseniGiri.Exceptions;
 
-//eccezione in caso si mette un top minore di un bottom sul between
+//eccezione in caso si mette un top minore di un bottom sul between.
 public class ExtremesException extends RuntimeException{
 	
 	private static final long serialVersionUID = 1L;
--- ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Filter/FavoritesFilter.java
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Filter/FavoritesFilter.java
@@ -3,6 +3,7 @@ package com.TiseniGiri.ProgettoTiseniGiri.Filter;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.TiseniGiri.ProgettoTiseniGiri.Exceptions.ExtremesException;
 import com.TiseniGiri.ProgettoTiseniGiri.Filter.Interfaces.NumericalFilter;
 import com.TiseniGiri.ProgettoTiseniGiri.Model.Tweet;
 
@@ -32,6 +33,9 @@ public class FavoritesFilter implements NumericalFilter {
 	}
 	@Override
 	public List<Tweet> between(List<Tweet> list, Object top, Object bottom){
+		if ((int)top <= (int)bottom)
+			throw new ExtremesException("The top value must be higher that the bottom value");
+		
 		List<Tweet> list1 = new ArrayList<Tweet>(list);
 		list1 = greater(list, bottom);
 		list1 = lower(list1,top);
