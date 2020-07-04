13a82c2bc15245d8e160310363a33106c0602633
Lorenzo Tiseni <s1087649@studenti.univpm.it>
2020-07-04 12:55:14 +0200

    Aggiunte nuove classi per eccezioni

---
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Exceptions/EmptyListException.java
@@ -0,0 +1,15 @@
+package com.TiseniGiri.ProgettoTiseniGiri.Exceptions;
+
+public class EmptyListException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EmptyListException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Exceptions/NegativeNumberException.java
@@ -0,0 +1,14 @@
+package com.TiseniGiri.ProgettoTiseniGiri.Exceptions;
+
+public class NegativeNumberException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NegativeNumberException(String message) {
+		super(message);
+	}
+
+}
