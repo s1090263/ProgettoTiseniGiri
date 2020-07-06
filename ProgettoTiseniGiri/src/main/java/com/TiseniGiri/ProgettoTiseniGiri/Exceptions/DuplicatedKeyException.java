3c00e78cf57f85f9cfd1964c6373ae1cf6832cec
Lorenzo Tiseni <s1087649@studenti.univpm.it>
2020-07-06 12:42:12 +0200

    Added DuplicatedKeyException

---
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Exceptions/DuplicatedKeyException.java
@@ -0,0 +1,14 @@
+package com.TiseniGiri.ProgettoTiseniGiri.Exceptions;
+
+public class DuplicatedKeyException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6L;
+
+	public DuplicatedKeyException(String message) {
+		super(message);
+	}
+
+}
