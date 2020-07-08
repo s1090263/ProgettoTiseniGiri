726a7a2d6da1575a1fd3a2312173f06f29a87b93
Matteo Giri <s1090263@studenti.univpm.it>
2020-07-08 13:15:10 +0200

    Added javaDoc

--- ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Utility/Downloader.java
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Utility/Downloader.java
@@ -7,37 +7,56 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
+/**
+ * Class that downloads the tweets from the API request
+ * 
+ * @author Lorenzo Tiseni
+ * @author Matteo Giri
+ */
 public class Downloader {
-	
-	public static String downloadData (String url) {
-		
-		String data = "";
-		String line = "";		
-			
+
+	/**
+	 * String representing the data downloaded
+	 */
+	private static String data = "";
+	/**
+	 * String containing the line that is being read
+	 */
+	private static String line = "";
+
+	/**
+	 * method that downloads the data from the API
+	 * 
+	 * @param url the url representing the request to the API
+	 * @return the data as a string
+	 */
+	public static String downloadData(String url) {
+
 		try {
 			URLConnection openConnection = new URL(url).openConnection();
-			openConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
+			openConnection.addRequestProperty("User-Agent",
+					"Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
 			InputStream input = openConnection.getInputStream();
-				
+
 			try {
-					InputStreamReader inputR = new InputStreamReader( input );
-					BufferedReader buffered = new BufferedReader( inputR );
-					  
-					while ( ( line = buffered.readLine() ) != null ) {
-						data+= line;
-						}
-					} catch (IOException e) {
-						System.out.println("Error: IOException found");	
-					} finally {
-						input.close();
-					}
-			}
-					
-			catch (IOException e) {
-				
+				InputStreamReader inputR = new InputStreamReader(input);
+				BufferedReader buffered = new BufferedReader(inputR);
+
+				while ((line = buffered.readLine()) != null) {
+					data += line;
+				}
+			} catch (IOException e) {
 				System.out.println("Error: IOException found");
+			} finally {
+				input.close();
 			}
-		
+		}
+
+		catch (IOException e) {
+
+			System.out.println("Error: IOException found");
+		}
+
 		return data;
 	}
 }
--- ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Utility/UrlSetter.java
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/Utility/UrlSetter.java
@@ -1,13 +1,29 @@
 package com.TiseniGiri.ProgettoTiseniGiri.Utility;
 
+/**
+ * Class that sets the correct url for the API request
+ * 
+ * @author Lorenzo Tiseni
+ * @author Matteo Giri
+ */
 public class UrlSetter {
-	private static String url = "https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/search/tweets.json?q=@UnivPoliMarche&count=100" ;
 
-	//getter con topic e numero, restituisce il numero di tweets richiesto
+	/**
+	 * Standard url
+	 */
+	private static String url = "https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/search/tweets.json?q=@UnivPoliMarche&count=100";
+
+	/**
+	 * Get the correct url with topic and num
+	 * 
+	 * @param topic the topic of the tweets to get
+	 * @param num   the number of tweets to get
+	 * @return the url
+	 */
 	public static String getUrl(String topic, int num) {
-		url = "https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/search/tweets.json?q=" + topic + "&count=" + num;
+		url = "https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/search/tweets.json?q=" + topic
+				+ "&count=" + num;
 		return url;
 	}
-	
-}
 
+}
