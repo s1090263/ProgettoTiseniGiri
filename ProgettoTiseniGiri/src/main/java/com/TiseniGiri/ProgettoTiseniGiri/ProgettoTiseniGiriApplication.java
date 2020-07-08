239759bd2550fca43b8661e93bc43387ff7c4b80
Matteo Giri <s1090263@studenti.univpm.it>
2020-07-08 13:26:07 +0200

    aggiornato main

--- ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/ProgettoTiseniGiriApplication.java
+++ ProgettoTiseniGiri/src/main/java/com/TiseniGiri/ProgettoTiseniGiri/ProgettoTiseniGiriApplication.java
@@ -1,12 +1,7 @@
 package com.TiseniGiri.ProgettoTiseniGiri;
 
-
-
-import java.lang.reflect.InvocationTargetException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 
 /**
  * Main class
@@ -17,8 +12,8 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 @SpringBootApplication
 public class ProgettoTiseniGiriApplication {
 
-	public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, SecurityException, JsonMappingException, JsonProcessingException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException{
-		
-		SpringApplication.run(ProgettoTiseniGiriApplication.class, args);			
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProgettoTiseniGiriApplication.class, args);
 	}
 }
